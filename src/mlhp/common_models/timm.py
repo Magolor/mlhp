237bd98168646845075a452b136494857654d0a1
陈醉 <chenzui@chenzuideMacBook-Pro.local>
2021-07-30 20:12:34 +0800

    0.1.0.0

--- src/mlhp/common_models/timm.py
+++ src/mlhp/common_models/timm.py
@@ -9,7 +9,7 @@ class TimmBackbone(nn.Module, TorchSerializable):
     module_abbr = "tim"
 
     def __init__(self, model_type="", module_name='timm', module_abbr='tim',
-        input_layer_name="conv1", modify_input_channels=None,
+        input_layer_getter=lambda n:n.conv1, modify_input_channels=None,
         output_layer_name="fc", embedding_dim=2048,
         **model_args
     ):
@@ -18,18 +18,18 @@ class TimmBackbone(nn.Module, TorchSerializable):
         self.module_abbr = module_abbr
         self.net = create_model(model_type, **model_args)
 
-        layer = getattr(self.net, input_layer_name); device = layer.weight.device
+        layer = input_layer_getter(self.net); device = layer.weight.device
         if modify_input_channels and modify_input_channels!=layer.in_channels:
             layer.in_channels = modify_input_channels
             shape = layer.weight.shape; shape = torch.Size([shape[0],modify_input_channels,shape[2],shape[3]])
             layer.weight = nn.Parameter(torch.zeros(shape)).to(device)
             nninit.xavier_uniform_(layer.weight)
-        layer = getattr(self.net, input_layer_name)
+        layer = input_layer_getter(self.net)
         self.in_channels = layer.in_channels
         
         self.net = IntermediateLayerGetter(self.net, return_layers={output_layer_name:"out"})
-        self.net.__dict__[output_layer_name] = nn.Linear(getattr(self.net,output_layer_name).in_features,embedding_dim,bias=True)
+        self.net._modules[output_layer_name] = nn.Linear(self.net._modules[output_layer_name].in_features,embedding_dim,bias=True)
         self.net.to(device)
 
     def forward(self, data):
-        return self.net(data)
+        return self.net(data)['out']
